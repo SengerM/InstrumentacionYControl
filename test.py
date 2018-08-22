864e6ba0a553bd945d55053f1498e14889ef5043
SengerM <m.senger@hotmail.com>
2018-08-22 18:57:07 -0300

    No sé por qué la frecuencia de audio no es la que debería ser...

--- test.py
+++ test.py
@@ -4,14 +4,11 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-CHUNK_SIZE = 1024
 SAMPLING_RATE = 44100
 AUDIO_FREQ = 1000
 
-samples = np.sin(2*np.pi*AUDIO_FREQ/SAMPLING_RATE*np.array(range(CHUNK_SIZE)))
+samples = np.sin(2*np.pi*AUDIO_FREQ/SAMPLING_RATE*np.array(range(int(np.floor(SAMPLING_RATE/AUDIO_FREQ)))))
 samples = samples.astype(np.float32)
-
-plt.plot(samples)
 
 p = pyaudio.PyAudio()
 
