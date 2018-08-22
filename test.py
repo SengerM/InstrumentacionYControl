7f5dd02509efa5c5a21fa95f3527d3940e52bd9d
Publico <Publico@laboratorios.df>
2018-08-22 18:04:36 -0300

    Estoy intentando instalar numpy, no se si esto funciona

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
-"""PyAudio Example: Play a WAVE file."""
-
 import pyaudio
 import wave
 import sys
+import numpy as np
 
 CHUNK_SIZE = 1024
 SAMPLING_RATE = 44100
+AUDIO_FREQ = 1e3
+
+samples = np.sin(2*np.pi*AUDIO_FREQ/SAMPLING_RATE*range(CHUNK_SIZE))
+samples = samples.astype(pyaudio.paInt16)
 
 p = pyaudio.PyAudio()
 
 ostream = p.open(format=pyaudio.paInt16, channels=2, rate=SAMPLING_RATE, output=True)
 
+while True:
+	ostream.write(samples)
 # data = wave_file.readframes(CHUNK)
 # while data != '':
     # ostream.write(data)
