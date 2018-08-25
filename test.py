e0fc6ed3b2debbb61e790c4d28ed326663659298
SengerM <m.senger@hotmail.com>
2018-08-25 09:57:33 -0300

    Comiteo basura pues sino no me deja pullear el master

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import pyaudio
-import wave
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,18 +8,13 @@ AUDIO_FREQ = 100
 chunk_size = int(SAMPLING_RATE/AUDIO_FREQ)
 samples = np.sin(2*np.pi*AUDIO_FREQ/SAMPLING_RATE*np.arange(chunk_size))
 samples = samples.astype(np.float32)
-
 plt.plot(samples)
 plt.show()
 
 p = pyaudio.PyAudio()
-
 ostream = p.open(format=pyaudio.paFloat32, channels=2, rate=SAMPLING_RATE, output=True)
-
-while True:
+for k in range(60):
 	ostream.write(samples)
-
 ostream.stop_stream()
 ostream.close()
-
 p.terminate()
