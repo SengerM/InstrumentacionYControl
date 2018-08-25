71f0250a186262e7387e2c14c5741d9b3acdb376
SengerM <m.senger@hotmail.com>
2018-08-25 11:01:36 -0300

    Ahora está funcionando con sounddevice. No se puede hacer el 'while (True): play(samples)' pues no funciona, hay un transitorio extraño en las placas de sonido. Hay que generar todo el choclo de muestras y luego aplicarlo de un saque. En este caso la señal de salida será parecida a lo que se envíe (habrán N muestras al principio y/o al final que no salen, no sé por qué).

--- osciloscopio.py
+++ osciloscopio.py
@@ -1,50 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 24 20:42:30 2018
-
-@author: admin
-"""
-
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import scipy.signal as signal
 
-volumen = 1.0       # rango en que se mueve [0.0, 1.0]
-fs = 48000          # sampling rate, Hz, debe ser entero
-f = 200.0           # frecuencia
-duracion = 2        # en segundos, duracion de la muestra
-#chunk=1024         por ahora no lo vamos a usar
-sd.default.samplerate = fs
-sd.default.channels = 1 
-funcion='cc'        
-#tipo de funcion con que vamos a exitar: sinusoideal,triangular,cc
+# Parameters ------------------------------------
+AMPLITUDE = 1.0 # Amplitude of the signal between 0 and 1.
+SIGNAL_FREQUENCY = 1000 # In Hertz.
+SIGNAL = 'ramp' # 'sin', 'ramp'
+N_CYCLES = 10 # This must be "a great number" to overcome a strange transitory of the sound card...
+SAMPLING_FREQUENCY = 48000 # Must be integer.
+# -----------------------------------------------
+if not isinstance(SAMPLING_FREQUENCY, int):
+	raise ValueError('ERROR: SAMPLING_FRECUENCY must be an integer number!')
 
-if funcion == 'sinusoideal':
-    #funcion sinusoideal de frecuencia f 
-    data = (np.sin(2*np.pi*np.arange(fs*duracion)*f/fs)).astype(np.float32)
-if funcion == 'triangular':
-    #funcion triangular de frecuencia f
-    simetria=0.5 #0.5 es simetrica de ambos lados
-    t= np.linspace(0,1,int(fs*duracion))
-    data = (signal.sawtooth(2 * np.pi * f * t,simetria)).astype(np.float32)
-if funcion == 'cc':
-    #funcion continua
-    data = np.linspace(volumen,volumen,int(fs*duracion))
- 
-#corre y graba la funcion data
-grabacion = sd.playrec(data*volumen)
+sd.default.samplerate = SIGNAL_FREQUENCY
+sd.default.channels = 1
 
-#esperamos a que termine
-time.sleep(duracion)
-
-#ploteamos la tension de entrada en funcion del tiempo
-t=np.linspace(0,duracion,int(fs*duracion))
-plt.plot(t,grabacion[:,0])
-
-#ploteamos la tension de entrada en funcion de la tension de salida
-plt.plot(data,grabacion[:,0])
-
-t=np.linspace(0,duracion,int(fs*duracion))
-plt.plot(t,data)
+if SIGNAL == 'sin':
+	samples = np.sin(2*np.pi*np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)*SIGNAL_FREQUENCY/SAMPLING_FREQUENCY)
+elif SIGNAL == 'ramp':
+	samples = np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)
+# Set the appropiate amplitude ---
+samples -= np.min(samples)
+samples /= np.max(samples)
+samples -= 0.5
+samples *= AMPLITUDE
+samples = samples.astype(np.float32)
+output_samples = samples
+# Create the output samples -----
+for k in range(N_CYCLES-1):
+	output_samples = np.append(output_samples, samples)
+# Play samples ------------------
+sd.play(output_samples, SAMPLING_FREQUENCY)
+time.sleep(N_CYCLES/SIGNAL_FREQUENCY)
