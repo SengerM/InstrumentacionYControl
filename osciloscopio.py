35e7733ca10a34bf5e06192ace13b11794290093
SengerM <m.senger@hotmail.com>
2018-08-27 09:18:55 -0300

    Agregué la señal cuadrada tal como lo hizo Alexis sólo que con algunos cambios menores para no replicar código.

--- osciloscopio.py
+++ osciloscopio.py
@@ -6,38 +6,47 @@ import scipy.signal as signal
 import utils.matplotlib_my_utils as mplt
 
 # Parameters ------------------------------------
-AMPLITUDE = 1.0 # Amplitude of the signal between 0 and 1.
+AMPLITUDE = 1 # Amplitude of the signal between 0 and 1.
+OFFSET = 0 # Signal offset between -1 and +1. 
 SIGNAL_FREQUENCY = 100 # In Hertz.
-SIGNAL = 'ramp' # 'sin', 'ramp'
+SIGNAL = 'squ' # 'sin', 'ramp', 'squ'
 N_CYCLES = 10 # This must be "a great number" to overcome a strange transitory of the sound card...
 SAMPLING_FREQUENCY = 48000 # Must be integer.
 # -----------------------------------------------
-figs = [] # Do not touch this.
+figs = [] # Do not touch this, ja!
+available_signals = ['sin','ramp','squ']
+# Validations -----------------------------------
 if not isinstance(SAMPLING_FREQUENCY, int):
-	raise ValueError('ERROR: SAMPLING_FRECUENCY must be an integer number!')
-
-sd.default.samplerate = SIGNAL_FREQUENCY
-sd.default.channels = 1
-
+	raise ValueError('SAMPLING_FRECUENCY must be an integer number!')
+if AMPLITUDE + OFFSET > 1:
+	raise ValueError('AMPLITUDE+OFFSET must be less than 1')
+if available_signals.count(SIGNAL) == 0:
+	raise ValueError('Invalid type of signal. Parameter "SIGNAL" must be one of the following ' + str(available_signals))
+# Create the signal -----------------------------
 if SIGNAL == 'sin':
 	samples = np.sin(2*np.pi*np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)*SIGNAL_FREQUENCY/SAMPLING_FREQUENCY)
 elif SIGNAL == 'ramp':
 	samples = np.arange(SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)
-# Set the appropiate amplitude ---
+elif SIGNAL == 'squ':
+	samples = np.linspace(1,1,int((SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)/2))
+	samples = np.append(samples,np.linspace(0,0,int((SAMPLING_FREQUENCY/SIGNAL_FREQUENCY)/2)))
+# Set the appropiate amplitude and offset -------
 samples -= np.min(samples)
 samples /= np.max(samples)
 samples -= 0.5
 samples *= 2*AMPLITUDE
+samples += OFFSET
 samples = samples.astype(np.float32)
+# Create the output samples ---------------------
 output_samples = samples
-# Create the output samples -----
 for k in range(N_CYCLES-1):
 	output_samples = np.append(output_samples, samples)
-# Play and record samples -------
+# Play and record samples -----------------------
+sd.default.samplerate = SAMPLING_FREQUENCY
+sd.default.channels = 1
 recorded_samples = sd.playrec(output_samples, SAMPLING_FREQUENCY)
 time.sleep(N_CYCLES/SIGNAL_FREQUENCY)
-
-# PLOT ----------------------------
+# PLOT ------------------------------------------
 f, axes = plt.subplots(2, sharex=True, figsize=(mplt.fig_width*mplt.fig_ratio[0]/25.4e-3, mplt.fig_width*mplt.fig_ratio[1]/25.4e-3)) # Create the figure for plotting.
 f.subplots_adjust(hspace=0.3) # Fine-tune figure; make subplots close to each other and hide x ticks for all but bottom plot.
 figs.append(f)
@@ -48,7 +57,7 @@ for k in range(len(axes)):
 	axes[k].set_ylabel('Amplitude')
 	axes[k].legend()
 axes[-1].set_xlabel('Time (s)')
-# Save figures --------------------
+# Save figures -----------------------------------
 for k in range(len(figs)):
 	figs[k].savefig(str(k+1) + '.' + mplt.image_format, bbox_inches='tight', dpi=mplt.dpi_rasterization)
 
