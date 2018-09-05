11e6860e480c17a42fd8842cd635475705be02c3
Publico <Publico@laboratorios.df>
2018-09-05 18:29:53 -0300

    Aparentemente está funcionando la respuesta en frecuencia.

--- frequency_response.py
+++ frequency_response.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import utils.matplotlib_my_utils as mplt
+import scipy.optimize as opt
 import os
 os.system('cls')
 # Parameters ------------------------------------
-SIGNAL_FREQUENCIES = np.logspace(np.log10(100),np.log10(1000),5) # In Hertz.
+SIGNAL_FREQUENCIES = np.logspace(np.log10(100),np.log10(10e3),20) # In Hertz.
 # -----------------------------------------------
 figs = [] # Do not touch this, ja!
 
@@ -18,6 +19,7 @@ def frequency_response(frequencies, amplitude=1, measuring_cycles=100, rancius_t
 		raise ValueError('AMPLITUDE must be less than 1 because the output samples must lie between +-1')
 	
 	amplitudes = [None]*len(frequencies)
+	phase = [None]*len(frequencies)
 	for k in range(len(frequencies)):
 		samples = np.sin(2*np.pi*np.arange(sampling_frequency/frequencies[k]*(rancius_time*frequencies[k] + measuring_cycles))*frequencies[k]/sampling_frequency)
 		samples -= np.min(samples)
@@ -29,27 +31,41 @@ def frequency_response(frequencies, amplitude=1, measuring_cycles=100, rancius_t
 		recorded_samples = sd.playrec(samples, sampling_frequency, channels=2)
 		time.sleep(len(samples)/sampling_frequency+0.1) # The '0.1' extra was added because otherwise this does not work...
 		recorded_samples = np.transpose(recorded_samples)
-		amplitudes[k] = [None]*len(recorded_samples) # Left and right.
-		recorded_samples_temp = [None]*len(recorded_samples)
-		for l in range(len(recorded_samples)): # Left and right channels.
-			recorded_samples_temp[l] = recorded_samples[l][np.int(sampling_frequency*rancius_time):] # Discard rancius samples.
-			amplitudes[k][l] = np.max(recorded_samples_temp[l])
-		#	amplitudes[k][l] = 2*np.sum(recorded_samples_temp[l]**2)/len(recorded_samples_temp[l])
-	return np.transpose(amplitudes)
+		recorded_samples_fixed = [None]*2
+		params = [None]*2
+		for l in range(2): # Left and right channels.
+			recorded_samples_fixed[l] = recorded_samples[l][np.int(sampling_frequency*rancius_time):] # Discard rancius samples.
+			# Fit sine to data:
+			funcSin = lambda params,x : params[0]*np.sin(params[1]*x + params[2])
+			func = funcSin
+			ErrorFunc = lambda params,x,y: func(params,x)-y # ErrorFunc is the diference between the func and the y "experimental" data
+			params0 = (1.0,frequencies[k]/sampling_frequency*2*np.pi,0) # "First guess" of the parameters.
+			params[l],success = opt.leastsq(ErrorFunc,params0[:],args=(np.arange(len(recorded_samples_fixed[l])),recorded_samples_fixed[l]))
+		
+		# f, axes = plt.subplots(2, sharex=True, figsize=(mplt.fig_width*mplt.fig_ratio[0]/25.4e-3, mplt.fig_width*mplt.fig_ratio[1]/25.4e-3)) # Create the figure for plotting.
+		# f.subplots_adjust(hspace=0.3) # Fine-tune figure; make subplots close to each other and hide x ticks for all but bottom plot.
+		# figs.append(f)
+		# axes[0].plot(recorded_samples_fixed[0], color=mplt.colors[0], label='Data left')
+		# axes[0].plot(func(params[0],np.arange(len(recorded_samples_fixed[0]))), color=mplt.colors[1], label='Fit left')
+		# axes[1].plot(recorded_samples_fixed[1], color=mplt.colors[2], label='Data right')
+		# axes[1].plot(func(params[1],np.arange(len(recorded_samples_fixed[1]))), color=mplt.colors[3], label='Fit right')
+			
+		amplitudes[k] = np.max(recorded_samples_fixed[0])/np.max(recorded_samples_fixed[1])
+		phase[k] = params[0][2] - params[1][2] # Phase difference.
+	return np.array(amplitudes), np.array(phase)
 
-amplitudes = frequency_response(SIGNAL_FREQUENCIES)
-
-print(amplitudes)
+amplitude, phase = frequency_response(SIGNAL_FREQUENCIES)
 
 # PLOT ------------------------------------------
 f, axes = plt.subplots(2, sharex=True, figsize=(mplt.fig_width*mplt.fig_ratio[0]/25.4e-3, mplt.fig_width*mplt.fig_ratio[1]/25.4e-3)) # Create the figure for plotting.
 f.subplots_adjust(hspace=0.3) # Fine-tune figure; make subplots close to each other and hide x ticks for all but bottom plot.
 figs.append(f)
+axes[0].plot(SIGNAL_FREQUENCIES, amplitude, color=mplt.colors[0], label='Amplitude', marker='.')
+axes[1].plot(SIGNAL_FREQUENCIES, phase*180/np.pi, color=mplt.colors[1], label='Phase', marker='.')
 for k in range(len(axes)):
-	axes[k].plot(SIGNAL_FREQUENCIES, amplitudes[k], color=mplt.colors[k], label='Channel ' + str(k+1), marker='.')
 	mplt.beauty_grid(axes[k])
-	axes[k].set_ylabel('Amplitude')
-	axes[k].legend()
+axes[0].set_ylabel('Amplitude')
+axes[1].set_ylabel('Phase (deg)')
 axes[-1].set_xlabel('Time (s)')
 axes[-1].set_xscale('log')
 axes[-1].set_xlabel('Frequency (Hz)')
